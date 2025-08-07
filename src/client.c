44ebc3c57e7934f031402e5284e4027276166565
tomi0100 <tomi0100@mailbox.org>
2025-08-07 19:55:06 +0200

    added port

--- src/client.c
+++ src/client.c
@@ -8,6 +8,7 @@ int main()
 
     /* You can create your own environment variables and change their names and adapt them below */
     const char *host = getenv("PG_HOST");
+    const char *port = getenv("PG_PORT");
     const char *database = getenv("PG_DATABASE");
     const char *user = getenv("PG_USER");
     const char *password = getenv("PG_PASSWORD");

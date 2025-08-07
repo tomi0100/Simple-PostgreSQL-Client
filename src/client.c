372e29e63923c7c78a49d185511b87dc6c9c7684
tomi0100 <tomi0100@mailbox.org>
2025-08-07 19:41:57 +0200

    env variables for the connection

--- src/client.c
+++ src/client.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <libpq-fe.h>
 #include <ncurses.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
+
+    /* You can create your own environment variables and change their names and adapt them below */
+    const char *host = getenv("PG_HOST");
+    const char *database = getenv("PG_DATABASE");
+    const char *user = getenv("PG_USER");
+    const char *password = getenv("PG_PASSWORD");
 
     return 0;
 }
